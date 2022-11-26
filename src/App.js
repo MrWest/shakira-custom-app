62b7300510e0ed0f2bc21c8ae8f1fc766a01ed9c
MrWest <kidwest87@gmail.com>
2022-11-26 15:49:22 -0500

    feat: add shakira pic

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome dude. Hello world!...
         </p>
+        <img style={{ width: 232, height: 320, objectFit: 'contain' }}
+        src="https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTgwMTgyMTgzOTk3MDg4ODkw/gettyimages-483195065.jpg" alt="shakira" />
         <a
           className="App-link"
           href="https://reactjs.org"
